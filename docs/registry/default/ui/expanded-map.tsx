a1bfd4b01702fae470712752ba2bb74586aa1e61
Shejan Mahamud <mdshejanmahamud006@gmail.com>
2026-01-17 00:58:53 +0600

    ✨ feat: remove click hint from LocationMap component

--- docs/registry/default/ui/expanded-map.tsx
+++ docs/registry/default/ui/expanded-map.tsx
@@ -334,20 +334,6 @@ export function LocationMap({
         </div>
 
       </motion.div>
-
-      {/* Click hint */}
-      <motion.p
-        className="absolute -bottom-6 left-1/2 text-[10px] text-muted-foreground whitespace-nowrap"
-        style={{ x: "-50%" }}
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: isHovered && !isExpanded ? 1 : 0,
-          y: isHovered ? 0 : 4,
-        }}
-        transition={{ duration: 0.2 }}
-      >
-        Click to expand
-      </motion.p>
     </motion.div>
   )
 }
